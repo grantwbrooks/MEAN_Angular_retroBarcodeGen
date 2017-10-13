4e63a35bec1a5558f54929437d53427afde12f6d
grantwbrooks <grantwbrooks@gmail.com>
2017-10-12 20:24:01 -0400

    finished

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Blue Barcode Genny';
+  colors = shuffle(colors);
+}
+
+function shuffle(array): Array<string> {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+var colors: Array<string> = ['Aqua', 'Aquamarine', 'DarkCyan', 'DodgerBlue', 'LightBlue', 'LightSeaGreen']
